8e2c417e15d5cdad122704d126d6adc0f15f36a0
Cerkes Mirza <almas1610@gmail.com>
2021-11-30 07:49:46 +0600

    ngrx learn with counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'learn-ngrx';
+  counter = 0;
+  updatedAt?: number;
+
+  get offDecrease(): boolean {
+    return this.counter <= 0;
+  }
+
+  increase(): void {
+    this.updatedAt = Date.now();
+    this.counter++
+  }
+
+  decrease(): void {
+    this.counter--
+  }
+
+  clear(): void {
+    this.counter = 0;
+  }
 }
