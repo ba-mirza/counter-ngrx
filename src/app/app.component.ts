4bba2b9d962b8db3d664f59b44ca8078f7dc3def
Cerkes Mirza <almas1610@gmail.com>
2021-12-03 23:40:27 +0600

    Added NGRX for counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,37 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Store} from "@ngrx/store";
+import {clear, countSelector, decrease, increase} from "./reducers/counter";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  counter = 0;
+export class AppComponent{
   updatedAt?: number;
 
+  count$ = this.store.select(countSelector);
+  cannotDecrease$ = this.count$.pipe(map(count => count <= 0));
+
+  constructor(private store: Store) {
+  }
+
   get offDecrease(): boolean {
-    return this.counter <= 0;
+    return false;
   }
 
   increase(): void {
     this.updatedAt = Date.now();
-    this.counter++
+    this.store.dispatch(increase());
   }
 
   decrease(): void {
-    this.counter--
+    this.store.dispatch(decrease());
   }
 
   clear(): void {
-    this.counter = 0;
+    this.store.dispatch(clear());
   }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
+import {environment} from "../environments/environment.prod";
+import {AppEffects} from './app.effects';
+import {metaReducers, reducers} from './reducers';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot(reducers, {metaReducers}),
+    StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
+    EffectsModule.forRoot([AppEffects])
   ],
   providers: [],
   bootstrap: [AppComponent]
